f740a54c289dc201b5c61923dcbc6199378ab11d
Forkeh <forkeh@gmail.com>
2024-09-06 11:10:06 +0200

    feat: created entities and dBcontext

---
+++ backend/api/Models/Entities/Page.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models.Entities;
+
+public class Page
+{
+    [Key, DatabaseGenerated((DatabaseGeneratedOption.None))] 
+    [Column("title")] 
+    public string Title { get; set; } = null!;
+
+    [Column("url")]
+    public string Url { get; set; } = null!;
+
+    [Column("language")]
+    public string Language { get; set; } = null!;
+
+    [Column("content")]
+    public string Content { get; set; } = null!;
+    
+    // https://www.entityframeworktutorial.net/faq/set-created-and-modified-date-in-efcore.aspx
+    [Column("last_updated")] 
+    public DateTime? LastUpdated { get; set; }
+}
---
+++ backend/api/Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models.Entities;
+
+[Index(nameof(Username), IsUnique = true)]
+[Index(nameof(Email), IsUnique = true)]
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+
+    public string Password { get; set; } = null!;
+}
