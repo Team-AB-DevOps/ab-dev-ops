528f4e9bc5eb0c08ae325db4b6dce969cc9a7d0f
Forkeh <forkeh@gmail.com>
2024-09-06 11:43:18 +0200

    feat: got DB working with .env variable

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,4 +1,9 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
 
 // Add services to the container.
 
@@ -6,6 +11,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DB CONNECTION
+var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception(
+        "Connection string not found. Ensure the .env file is correctly configured and placed in the root directory.");
+}
+
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors();
+});
 
 var app = builder.Build();
 
