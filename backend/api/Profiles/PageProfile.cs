49804b285bd36c1abeb65dc1585ee4034153c692
Forkeh <forkeh@gmail.com>
2024-09-07 19:24:52 +0200

    refactor: uses AutoMapper to map to Page DTO

---
+++ backend/api/Profiles/PageProfile.cs
@@ -0,0 +1,13 @@
+﻿using api.Models.DTOs;
+using api.Models.Entities;
+using AutoMapper;
+
+namespace api.Profiles;
+
+public class PageProfile : Profile
+{
+    public PageProfile()
+    {
+        CreateMap<Page, PageResponseDto>();
+    }
+}
