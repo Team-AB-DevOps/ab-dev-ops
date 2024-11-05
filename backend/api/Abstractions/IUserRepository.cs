e80f819744ba27a5ca342ebf05588e85607381a5
Forkeh <forkeh@gmail.com>
2024-11-05 15:10:54 +0100

    megalint

--- backend/api/Abstractions/IUserRepository.cs
+++ backend/api/Abstractions/IUserRepository.cs
@@ -4,9 +4,9 @@ namespace api.Abstractions;
 
 public interface IUserRepository
 {
-    Task<User?> GetByUsername(string username);
-    Task<User?> GetByEmail(string email);
-    Task<User> CreateUser(User user);
-    Task<User?> GetById(int id);
-    Task SaveChangesAsync();
+	Task<User?> GetByUsername(string username);
+	Task<User?> GetByEmail(string email);
+	Task<User> CreateUser(User user);
+	Task<User?> GetById(int id);
+	Task SaveChangesAsync();
 }
--- backend/api/Repositories/UserRepository.cs
+++ backend/api/Repositories/UserRepository.cs
@@ -7,37 +7,37 @@ namespace api.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private readonly DataContext _dataContext;
+	private readonly DataContext _dataContext;
 
-    public UserRepository(DataContext dataContext)
-    {
-        _dataContext = dataContext;
-    }
+	public UserRepository(DataContext dataContext)
+	{
+		_dataContext = dataContext;
+	}
 
-    public async Task<User?> GetByUsername(string username)
-    {
-        return await _dataContext.Users.FirstOrDefaultAsync(u => u.Username == username);
-    }
+	public async Task<User?> GetByUsername(string username)
+	{
+		return await _dataContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+	}
 
-    public async Task<User?> GetByEmail(string email)
-    {
-        return await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == email);
-    }
+	public async Task<User?> GetByEmail(string email)
+	{
+		return await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+	}
 
-    public async Task<User> CreateUser(User user)
-    {
-        var createdUser = await _dataContext.Users.AddAsync(user);
-        await SaveChangesAsync();
-        return createdUser.Entity;
-    }
+	public async Task<User> CreateUser(User user)
+	{
+		var createdUser = await _dataContext.Users.AddAsync(user);
+		await SaveChangesAsync();
+		return createdUser.Entity;
+	}
 
-    public async Task<User?> GetById(int id)
-    {
-        return await _dataContext.Users.FirstOrDefaultAsync(u => u.Id == id);
-    }
+	public async Task<User?> GetById(int id)
+	{
+		return await _dataContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+	}
 
-    public async Task SaveChangesAsync()
-    {
-        await _dataContext.SaveChangesAsync();
-    }
+	public async Task SaveChangesAsync()
+	{
+		await _dataContext.SaveChangesAsync();
+	}
 }
