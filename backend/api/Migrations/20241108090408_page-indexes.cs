2603d1a709216d5c220210d026421c52fcbb96ae
Ali Mohammad <113783570+AliHMohammad@users.noreply.github.com>
2024-11-15 09:05:20 +0100

    feat: added indexes for page table (#108)
    * feat: added indexes for page table
    
    * chore: lint

---
+++ backend/api/Migrations/20241108090408_page-indexes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+	/// <inheritdoc />
+	public partial class pageindexes : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateIndex(name: "Index_Title", table: "pages", column: "title");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropIndex(name: "Index_Title", table: "pages");
+		}
+	}
+}
